63307c7272e2ea77b2d39e4442828f1d6998d0d8
Asri Kinanti Febriany <72858439+febongz@users.noreply.github.com>
2021-05-03 21:18:40 +0700

    Add files via upload

---
+++ praktek8PHP/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$database="pendaftaran_siswa";
+// perintah php untuk akses ke database
+$koneksi=mysqli_connect($host, $user, $password, $database);
+?>
